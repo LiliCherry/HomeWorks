b9e353ec9524c2fbd59b4f3574065eae5b159cc5
Liliia Koliesnik <lulu.ko@outlook.com>
2020-11-01 13:06:02 +0200

    Added lesson1 task1

---
+++ lesson1/task1.1.js
@@ -0,0 +1,37 @@
+var a = 8;
+
+if (a === 1) {
+    console.log('один');
+}
+
+if (a === 2) {
+    console.log('два');
+}
+
+if (a === 3) {
+    console.log('три');
+}
+
+if (a === 4) {
+    console.log('четыре');
+}
+
+if (a === 5) {
+    console.log('пять');
+}
+
+if (a === 6) {
+    console.log('шесть');
+}
+
+if (a === 7) {
+    console.log('семь');
+}
+
+if (a === 8) {
+    console.log('восемь');
+}
+
+if (a === 9) {
+    console.log('девять');
+}
---
+++ lesson1/task1.2.js
@@ -0,0 +1,31 @@
+var a = 7;
+
+switch (a) {
+    case 1:
+        console.log('один');
+        break;
+    case 2:
+        console.log('два');
+        break;
+    case 3:
+        console.log('три');
+        break;
+    case 4:
+        console.log('четыре');
+        break;
+    case 5:
+        console.log('пять');
+        break;
+    case 6:
+        console.log('шесть');
+        break;
+    case 7:
+        console.log('семь');
+        break;
+    case 8:
+        console.log('восемь');
+        break;
+    case 9:
+        console.log('девять');
+        break;
+}
